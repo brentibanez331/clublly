c6db0ade0d5be0dd84421c430547f5c7d1a3c86d
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2025-04-27 21:29:38 +0800

    refactor: add product variant option values obj

--- lib/models/product_variant.dart
+++ lib/models/product_variant.dart
@@ -1,11 +1,18 @@
 // IGNORE FOR NOW!!!
 
+import 'dart:developer';
+
+import 'package:clublly/models/option_value.dart';
+import 'package:clublly/models/product_variant_option_values.dart';
+
 class ProductVariant {
   final int? id;
   final int productId;
   final num price;
   final num stockQuantity;
   final String? createdAt;
+  // final List<OptionValue>? optionValues;
+  final List<ProductVariantOptionValues>? productVariantOptionValues;
 
   ProductVariant({
     this.id,
@@ -13,15 +20,23 @@ class ProductVariant {
     required this.productId,
     required this.stockQuantity,
     this.createdAt,
+    this.productVariantOptionValues,
   });
 
   factory ProductVariant.fromMap(Map<String, dynamic> map) {
+    // List all option values
+    final List<ProductVariantOptionValues> pvovs =
+        (map['productVariantOptionValues'] as List)
+            .map((pvov) => ProductVariantOptionValues.fromMap(pvov))
+            .toList();
+
     return ProductVariant(
       id: map['id'],
       price: map['price'],
       productId: map['product_id'],
       stockQuantity: map['stock_quantity'],
       createdAt: map['created_at'],
+      productVariantOptionValues: pvovs,
     );
   }
 
