5a116c937f417b9674c0fa8f59372b0233dde77e
brentibanez331 <94149036+brentibanez331@users.noreply.github.com>
2025-05-05 20:51:47 +0800

    refactor: improve order model

--- lib/models/order.dart
+++ lib/models/order.dart
@@ -1,43 +1,63 @@
 class Order {
-  final int id;
+  final int? id;
   final String referenceCode;
   final num quantity;
   final num totalAmount;
   final String status;
-  final String releaseDate;
+  final String? releaseDate;
   final int pickupLocationId;
   final int productId;
-  final String createdAt;
-  final String updatedAt;
+  final String? createdAt;
+  final String? updatedAt;
   final String buyerId;
+  final int productVariantId;
+  final String paymentMethod;
 
   Order({
-    required this.id,
+    this.id,
     required this.referenceCode,
     required this.quantity,
     required this.buyerId,
     required this.pickupLocationId,
     required this.productId,
-    required this.releaseDate,
+    this.releaseDate,
     required this.status,
     required this.totalAmount,
-    required this.updatedAt,
-    required this.createdAt,
+    this.updatedAt,
+    this.createdAt,
+    required this.productVariantId,
+    required this.paymentMethod,
   });
 
   factory Order.fromMap(Map<String, dynamic> map) {
     return Order(
       id: map['id'],
       referenceCode: map['reference_code'],
+      quantity: map['quantity'],
+      totalAmount: map['total_amount'],
+      status: map['status'],
       buyerId: map['buyer_id'],
       pickupLocationId: map['pickup_location_id'],
       productId: map['product_id'],
-      quantity: map['quantity'],
       releaseDate: map['release_date'],
-      status: map['status'],
-      totalAmount: map['total_amount'],
       createdAt: map['created_at'],
       updatedAt: map['updated_at'],
+      productVariantId: map['product_variant_id'],
+      paymentMethod: map['payment_method'],
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'reference_code': referenceCode,
+      'quantity': quantity,
+      'total_amount': totalAmount,
+      'status': status,
+      'pickup_location_id': pickupLocationId,
+      'product_id': productId,
+      'buyer_id': buyerId,
+      'product_variant_id': productVariantId,
+      'payment_method': paymentMethod,
+    };
   }
 }
